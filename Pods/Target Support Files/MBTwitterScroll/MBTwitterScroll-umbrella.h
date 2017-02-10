794c8f6284a3cbdb0b597ef2395e6d596c908f7c
Igor Kuznetsov <iforaa@yandex.ru>
2017-02-10 18:39:16 +0300

    pod updates

---
+++ Pods/Target Support Files/MBTwitterScroll/MBTwitterScroll-umbrella.h
@@ -0,0 +1,8 @@
+#import <UIKit/UIKit.h>
+
+#import "MBTwitterScroll.h"
+#import "UIScrollView+TwitterCover.h"
+
+FOUNDATION_EXPORT double MBTwitterScrollVersionNumber;
+FOUNDATION_EXPORT const unsigned char MBTwitterScrollVersionString[];
+
